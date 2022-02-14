00227e19ac1fbc70286c107c591bab9301f457d9
Rob Lyon <rob@ctxswitch.com>
2022-02-13 20:48:18 -0800

    add helpers

---
+++ helpers_test.go
@@ -0,0 +1,27 @@
+package apex
+
+import (
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func buildProm(t *testing.T, name string, help string, ctype string, labels map[string]string, value float64) string {
+	var builder strings.Builder
+	builder.WriteString("# HELP ")
+	builder.WriteString(name + " ")
+	builder.WriteString(help + "\n")
+	builder.WriteString("# TYPE ")
+	builder.WriteString(name + " ")
+	builder.WriteString(ctype + "\n")
+	builder.WriteString(name + "{")
+	for k, v := range labels {
+		builder.WriteString(k + "=\"")
+		builder.WriteString(v + "\"} ")
+	}
+
+	val := strconv.FormatFloat(value, 'E', -1, 64)
+	builder.WriteString(val + "\n")
+
+	return builder.String()
+}
