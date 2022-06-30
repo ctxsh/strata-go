090ace7d6c9ba8585579ebaf1da196428aa7182a
Rob Lyon <rob@ctxswitch.com>
2022-06-30 08:28:27 -0700

    Move error to path root and start comments

--- metrics.go
+++ metrics.go
@@ -1,10 +1,27 @@
+// Copyright (C) 2022, Rob Lyon <rob@ctxswitch.com>
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 package apex
 
 import (
 	"fmt"
 	"net/http"
 
-	"ctx.sh/apex/errors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -25,7 +42,7 @@ type Metrics struct {
 	gauges     *Gauges
 	histograms *Histograms
 	summaries  *Summaries
-	errors     *errors.ApexInternalErrorMetrics
+	errors     *ApexInternalErrorMetrics
 }
 
 func New(opts MetricsOpts) *Metrics {
@@ -36,7 +53,7 @@ func New(opts MetricsOpts) *Metrics {
 		summaries:  NewSummaries(opts.Namespace, opts.Subsystem, opts.Separator),
 	}
 	m.opts = defaults(opts)
-	m.errors = errors.NewApexInternalErrorMetrics(opts.Namespace, opts.Subsystem, opts.Separator)
+	m.errors = NewApexInternalErrorMetrics(opts.Namespace, opts.Subsystem, opts.Separator)
 	return m
 }
 
@@ -169,11 +186,11 @@ func (m *Metrics) emitError(err error, name string, fn string) {
 	}
 
 	switch err {
-	case errors.ErrInvalidMetricName:
+	case ErrInvalidMetricName:
 		m.errors.InvalidMetricName(name, fn)
-	case errors.ErrRegistrationFailed:
+	case ErrRegistrationFailed:
 		m.errors.RegistrationFailed(name, fn)
-	case errors.ErrAlreadyRegistered:
+	case ErrAlreadyRegistered:
 		m.errors.AlreadyRegistered(name, fn)
 	}
 }
--- util.go
+++ util.go
@@ -1,9 +1,26 @@
+// Copyright (C) 2022, Rob Lyon <rob@ctxswitch.com>
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 package apex
 
 import (
 	"strings"
 
-	"ctx.sh/apex/errors"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
@@ -21,7 +38,7 @@ func NameBuilder(ns string, sub string, name string, sep rune) (string, error) {
 	}
 
 	if name == "" {
-		return "", errors.ErrInvalidMetricName
+		return "", ErrInvalidMetricName
 	}
 	builder.WriteString(name)
 	return builder.String(), nil
@@ -30,9 +47,9 @@ func NameBuilder(ns string, sub string, name string, sep rune) (string, error) {
 func Register(metric prometheus.Collector) error {
 	if err := prometheus.Register(metric); err != nil {
 		if _, ok := err.(prometheus.AlreadyRegisteredError); ok {
-			return errors.ErrAlreadyRegistered
+			return ErrAlreadyRegistered
 		} else {
-			return errors.ErrRegistrationFailed
+			return ErrRegistrationFailed
 		}
 	}
 	return nil
