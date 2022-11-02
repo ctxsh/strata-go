c18e02775de72e25f695cc7b8c6245fd47b34e54
Rob Lyon <rob@ctxswitch.com>
2022-11-01 21:06:05 -0700

    fix more tests

--- summary_test.go
+++ summary_test.go
@@ -9,7 +9,9 @@ import (
 
 func TestSummary(t *testing.T) {
 	reg := prometheus.NewPedanticRegistry()
-	vec, err := NewSummaryVec(reg, "test_smy", SummaryOpts{})
+	vec, err := NewSummaryVec(reg, "test_smy", SummaryOpts{
+		Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+	})
 	assert.NoError(t, err)
 
 	vec.Observe(10.0)
